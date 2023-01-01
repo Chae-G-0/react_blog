8642fd91a18a83533aa6e8d3543bf4d4b9e2e584
Chae-G-0 <corkdud@naver.com>
2023-01-02 00:15:44 +0900

    230102

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(){
+ 
+  const [skill, setSkill] = useState(['JavaScript', 'React', 'MongoDB']);
+  const [good, setGood] = useState(0);
+
   return (
     <div className="App">
-
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h4>
+          {skill[0]} <span onClick={()=>{setGood(good+1)}}>👍</span> {good}{" "}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{skill[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{skill[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
